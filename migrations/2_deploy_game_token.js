68de2d3e5383a7f17c8d2a949e87971a7629990d
okada-shun <syun19981207@gmail.com>
2021-10-15 15:10:09 +0900

    Make it possible to write address and abi to files

--- migrations/2_deploy_game_token.js
+++ migrations/2_deploy_game_token.js
@@ -1,6 +1,14 @@
 const GameToken = artifacts.require("./GameToken.sol");
+const fs = require('fs')
 
 module.exports = function(deployer) {
   const initialSupply = 10000;
-  deployer.deploy(GameToken, initialSupply);
+  deployer.deploy(GameToken, initialSupply).then(instance => {
+    try {
+      fs.writeFileSync('../game-api/GameToken_address.txt', instance.address);
+      fs.writeFileSync('../game-api/GameToken.abi', JSON.stringify(instance.abi));
+    } catch (err) {
+      console.log(err);
+    }
+  });
 };
