e20f5c835826d241d67e683e0ac323f21340d936
okada-shun <syun19981207@gmail.com>
2021-10-27 15:44:52 +0900

    Update GameToken constructor

--- contracts/GameToken.sol
+++ contracts/GameToken.sol
@@ -7,11 +7,27 @@ import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 //Solidity 0.5
 contract GameToken is ERC20Burnable, ERC20Mintable {
 
-  string public name = "GameToken";
-  string public symbol = "GT";
-  uint public decimals = 0;
+  string private _name;
+  string private _symbol;
+  uint8 private _decimals;
 
-  constructor(uint256 initialSupply) public {
+  constructor(uint256 initialSupply, string memory name, string memory symbol, uint8 decimals) public {
     _mint(msg.sender, initialSupply);
+    _name = name;
+    _symbol = symbol;
+    _decimals = decimals;
   }
+
+  function name() public view returns (string memory) {
+    return _name;
+  }
+
+  function symbol() public view returns (string memory) {
+    return _symbol;
+  }
+
+  function decimals() public view returns (uint8) {
+    return _decimals;
+  }
+
 }
